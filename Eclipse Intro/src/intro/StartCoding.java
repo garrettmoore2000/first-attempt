d26eb5417bc7d5b76192bbaee2f436fbffbacdf5
garre <garre@LAPTOP-4PFEP0IQ>
2020-09-07 20:25:11 -0400

    Nothing changed.

---
+++ Eclipse Intro/src/intro/StartCoding.java
@@ -0,0 +1,11 @@
+package intro;
+
+public class StartCoding 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hi, Dave!");
+		System.out.println("Hi, Larry!");
+	}
+}
